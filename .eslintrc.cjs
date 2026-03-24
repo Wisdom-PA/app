3e15fd8a2c9aa1bf21cc935e266b756bb3f1e606
NiallAllison <niallallison2@gmail.com>
2026-03-24 18:36:10 +0000

    app continued

---
+++ .eslintrc.cjs
@@ -0,0 +1,43 @@
+/** @type {import('eslint').Linter.Config} */
+module.exports = {
+  root: true,
+  env: {
+    es2022: true,
+    browser: true,
+    jest: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: { jsx: true },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint', 'react', 'react-hooks'],
+  settings: {
+    react: { version: 'detect' },
+  },
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+    'react/prop-types': 'off',
+    '@typescript-eslint/no-unused-vars': [
+      'error',
+      { argsIgnorePattern: '^_', varsIgnorePattern: '^_' },
+    ],
+    '@typescript-eslint/no-require-imports': 'off',
+  },
+  ignorePatterns: [
+    'node_modules/',
+    'coverage/',
+    '.expo/',
+    'dist/',
+    'build/',
+    'babel.config.js',
+    'jest.config.js',
+  ],
+};
